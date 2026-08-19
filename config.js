fee5bd8af961dd683a71499d8f01bfee91b36657
Exottiicz <masongts8@gmail.com>
2026-08-19 01:44:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// Paste your values from Supabase Project Settings > API to make this live.
-// The browser-safe anon key is fine here. Never place a service_role key in this file.
 window.BREAKLINE_CONFIG = {
   supabaseUrl: '',
   supabaseAnonKey: ''
