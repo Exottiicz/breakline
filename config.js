db43a418d1cea3e9e794e445253b2cf6a4f406b2
Exottiicz <masongts8@gmail.com>
2026-08-19 03:38:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.BREAKLINE_CONFIG = {
-  supabaseUrl: 'https://cxfivucsqpiizrjurcmz.supabase.co',
-  supabaseAnonKey: 'sb_publishable_Qbnw1SfcFXEhrrCH-t3a_w_Zm6cQneW'
+  supabaseUrl: 'https://tdqhwuwcemwefdwqphmy.supabase.co',
+  supabaseAnonKey: 'sb_publishable_ukS2JGRk77fmHncaSYufgg_y4LMcX4G'
 };
